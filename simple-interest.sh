298144a2dc4021c2d5ebc4a12e2c1831de789976
MohamedTarek <140357718+MohamedTarek-MTA@users.noreply.github.com>
2026-07-06 16:40:24 +0300

    Enhance comments and add author details in script
    Added author information and improved comments for clarity.

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,21 +3,29 @@
 # annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
 
-# Input:
-# p, principal amount
-# t, time period in years
-# r, annual rate of interest
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# <your GitHub username>
 
-# Output:
-# simple interest = p*t*r
-
+# Prompt the user for the principal amount
 echo "Enter the principal:"
+# Read the user input and store it in variable 'p'
 read p
+
+# Prompt the user for the annual rate of interest
 echo "Enter rate of interest per year:"
+# Read the user input and store it in variable 'r'
 read r
+
+# Prompt the user for the time period in years
 echo "Enter time period in years:"
+# Read the user input and store it in variable 't'
 read t
 
+# Calculate simple interest using the formula: (p * t * r) / 100
+# The 'expr' command evaluates the arithmetic expression
 s=`expr $p \* $t \* $r / 100`
+
+# Output the calculated simple interest to the user
 echo "The simple interest is: "
 echo $s
